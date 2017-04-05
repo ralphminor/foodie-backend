5015b67893982fae40325dbd180117e31fedf03f
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2017-04-05 17:46:38 -0600

    Added foodie for mongo uri

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import {MONGODB_URI} from 'react-native-dotenv';
 export default () => {
   mongoose.Promise = global.Promise;
-  mongoose.connect(MONGODB_URI);
+  mongoose.connect(MONGODB_URI + '/foodie');
   mongoose.connection
     .once('open', () => console.log('Mongodb running'))
     .on('error', err => console.error(err))
