4dbf68eff390cafee13167a42a6eaf2235e806e4
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2017-04-05 14:53:20 -0600

    Added heroku db details in .env.  Configured db.js.

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-
+import {MONGODB_URI} from 'react-native-dotenv';
 export default () => {
   mongoose.Promise = global.Promise;
-  mongoose.connect('mongodb://localhost/foodie');
+  mongoose.connect(MONGODB_URI + '/foodie');
   mongoose.connection
     .once('open', () => console.log('Mongodb running'))
     .on('error', err => console.error(err))
