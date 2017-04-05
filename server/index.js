01aa21def8d38a17c443f19ee9e7c849bde33a6a
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2017-04-05 11:56:21 -0600

    Configured mongodb and added middleware.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import dbConfig from './config/db';
+import middlewareConfig from './config/middleware';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+dbConfig();
+middlewareConfig(app);
 
 app.listen(PORT, err => {
   if (err) {
