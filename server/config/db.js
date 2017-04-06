300272afb2d362723f3dc15a7cc6dc8d907ee5c5
Ralph Minor <ralphminor@Ralphs-MacBook-Pro.local>
2017-04-05 20:47:56 -0600

    Removed /foodie

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-import {MONGODB_URI} from 'react-native-dotenv';
+import { MONGODB_URI } from 'react-native-dotenv';
 export default () => {
   mongoose.Promise = global.Promise;
-  mongoose.connect(MONGODB_URI + '/foodie');
+  mongoose.connect(MONGODB_URI);
   mongoose.connection
     .once('open', () => console.log('Mongodb running'))
     .on('error', err => console.error(err))
